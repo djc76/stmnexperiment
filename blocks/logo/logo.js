94b6b9f75f2f6ede57870acdbdc754ad92b8baf0
Daniel Huser <daniel.huser@straumann.com>
2025-04-23 15:43:09 +0200

    WIP: experimenting

---
+++ blocks/logo/logo.js
@@ -0,0 +1,17 @@
+
+export default async function decorate(block) {
+    const cols = [...block.firstElementChild.children];
+    block.textContent = '';
+
+    let logoParent = block;
+    if (cols.length > 0 && cols[0].textContent && cols[0].textContent.trim() !== '') {
+        logoParent = document.createElement('a');
+        //logoParent.createAttribute("href", cols[0].textContent);
+        logoParent.href = cols[0].textContent;
+        block.append(logoParent);
+    }
+
+    const logoImage = document.createElement('img');
+    logoImage.src = '../../images/logo-black-transparent.png'; // Replace with actual logo path
+    logoParent.append(logoImage);
+}
