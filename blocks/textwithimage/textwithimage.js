9de6cbece86a1720e2bdcd68cca58e236b7b582b
Daniel Huser <danieljchuser@gmail.com>
2025-04-29 16:35:45 +0200

    added blocks for text-wrapping and iframe

---
+++ blocks/textwithimage/textwithimage.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+    const cols = [...block.firstElementChild.children];
+    console.dir(cols);
+    if(cols.length === 2){
+        cols[0].classList.add('img');
+        cols[1].classList.add('text');
+    }
+}
