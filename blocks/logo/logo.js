68e2f9613f10be5e055683e1cd76bdb54d673d83
Daniel Huser <danieljchuser@gmail.com>
2025-04-30 16:14:28 +0200

    added alt/aria texts

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -6,10 +6,12 @@ export default async function decorate(block) {
   if (cols.length > 0 && cols[0].textContent && cols[0].textContent.trim() !== '') {
     logoParent = document.createElement('a');
     logoParent.href = cols[0].textContent;
+    logoParent.setAttribute('aria-label', 'Hyperlink to Home of Pistolensektion Laufen');
     block.append(logoParent);
   }
 
   const logoImage = document.createElement('img');
   logoImage.src = '../../images/logo-black-transparent.png'; // Replace with actual logo path
+  logoImage.alt = 'Pistolensektion Laufen Logo';
   logoParent.append(logoImage);
 }
