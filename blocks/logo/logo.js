3e3d0058d5649f0207f4cb4ce31b439af8901565
Daniel Huser <danieljchuser@gmail.com>
2025-04-30 14:37:55 +0200

    added titlebanner block

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -5,7 +5,6 @@ export default async function decorate(block) {
   let logoParent = block;
   if (cols.length > 0 && cols[0].textContent && cols[0].textContent.trim() !== '') {
     logoParent = document.createElement('a');
-    //  logoParent.createAttribute("href", cols[0].textContent);
     logoParent.href = cols[0].textContent;
     block.append(logoParent);
   }
